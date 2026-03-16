ef8e2a6062afbbb84fa23f659651d1c24f823a77
DanielxVision <dc.chauhan0211@gmail.com>
2026-03-16 18:54:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function scrollToTutorials(){
+particlesJS("particles-js", {
+particles: {
+number: { value: 80 },
+color: { value: "#ffffff" },
+shape: { type: "circle" },
+opacity: { value: 0.2 },
+size: { value: 3 },
+move: {
+enable: true,
+speed: 1
+}
+},
+interactivity: {
+events: {
+onhover: {
+enable: true,
+mode: "repulse"
+}
+}
+}
+});
 
+function scrollToTutorials(){
 document.getElementById("tutorials").scrollIntoView({
 behavior:"smooth"
 })
-
 }
