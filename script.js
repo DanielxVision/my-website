cca636e82691cced7b00806228e9439fe69a3c62
DanielxVision <dc.chauhan0211@gmail.com>
2026-03-16 19:44:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,49 @@ navigator.clipboard.writeText(text);
 
 alert("Script copied!");
 }
+let scripts = {
+
+script1: `-- Gamepass Script
+local MarketplaceService = game:GetService("MarketplaceService")
+
+print("Gamepass system loaded")`,
+
+script2: `-- Door Script
+script.Parent.Touched:Connect(function(hit)
+script.Parent.Transparency = 1
+end)`,
+
+script3: `-- Leaderboard Script
+game.Players.PlayerAdded:Connect(function(player)
+
+local stats = Instance.new("Folder")
+stats.Name = "leaderstats"
+stats.Parent = player
+
+end)`
+
+};
+
+function openScript(id){
+
+document.getElementById("scriptViewer").style.display = "flex";
+
+document.getElementById("scriptCode").innerText = scripts[id];
+
+}
+
+function closeScript(){
+
+document.getElementById("scriptViewer").style.display = "none";
+
+}
+
+function copyScript(){
+
+let text = document.getElementById("scriptCode").innerText;
+
+navigator.clipboard.writeText(text);
+
+alert("Script copied!");
+
+}
